7cd8286f44440939ba1a8d9463630422e3f6496f
ZeroDayTea <patrick.dobranowski@gmail.com>
2022-12-20 21:41:47 -0700

    func to spawn transparent window for windows OS

--- TransparencyWindowLibrary/TransaprencyWindowLibraryWindows.cpp
+++ TransparencyWindowLibrary/TransaprencyWindowLibraryWindows.cpp
@@ -1,2 +1,10 @@
 #include <windows.h>
+#include <string>
 
+HWND createWindow(std::string windowName, bool transparent, bool clickthrough) {
+	std::wstring stemp = std::wstring(windowName.begin(), windowName.end());
+	LPCWSTR title = stemp.c_str();
+	HWND hWnd = CreateWindowEx(WS_EX_LAYERED | WS_EX_TRANSPARENT, title, title, NULL, 0, 0, 640, 480, NULL, 0, GetModuleHandle(NULL), 0);
+	SetLayeredWindowAttributes(hWnd, 0, 100, LWA_ALPHA);
+	return hWnd;
+}
