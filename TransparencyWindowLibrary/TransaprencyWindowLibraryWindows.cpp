e2f6db4466e8e7b88a57c3dcab0d0b9dafd587f2
ZeroDayTea <patrick.dobranowski@gmail.com>
2022-12-21 18:38:22 -0700

    added drawing images for Windows

--- TransparencyWindowLibrary/TransaprencyWindowLibraryWindows.cpp
+++ TransparencyWindowLibrary/TransaprencyWindowLibraryWindows.cpp
@@ -1,10 +1,33 @@
 #include <windows.h>
 #include <string>
 
-HWND createWindow(std::string windowName, bool transparent, bool clickthrough) {
+HWND hWnd;
+
+void createWindow(std::string windowName, bool transparent, bool clickthrough) {
 	std::wstring stemp = std::wstring(windowName.begin(), windowName.end());
 	LPCWSTR title = stemp.c_str();
-	HWND hWnd = CreateWindowEx(WS_EX_LAYERED | WS_EX_TRANSPARENT, title, title, NULL, 0, 0, 640, 480, NULL, 0, GetModuleHandle(NULL), 0);
+
+	int height = GetSystemMetrics(SM_CYSCREEN);
+	int width = GetSystemMetrics(SM_CXSCREEN);
+
+	hWnd = CreateWindowEx(WS_EX_COMPOSITED | WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOPMOST, title, title, NULL, 0, 0, width, height, NULL, 0, GetModuleHandle(NULL), 0);
 	SetLayeredWindowAttributes(hWnd, 0, 100, LWA_ALPHA);
-	return hWnd;
+	ShowWindow(hWnd, SW_SHOW);
 }
+
+void drawImage(std::string image, int x, int y) {
+	std::wstring stemp = std::wstring(image.begin(), image.end());
+	LPCWSTR imagePath = stemp.c_str();
+
+	HDC hdc = GetDC(hWnd);
+	HDC hdcMem = CreateCompatibleDC(hdc);
+	HBITMAP hbm = (HBITMAP)LoadImage(NULL, imagePath, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+	HGDIOBJ old_obj = SelectObject(hdcMem, hbm);
+	BITMAP bm;
+	GetObject(hbm, sizeof(bm), &bm);
+	BitBlt(hdc, x, y, bm.bmWidth, bm.bmHeight, hdcMem, 0, 0, SRCCOPY);
+	SelectObject(hdcMem, old_obj);
+	DeleteDC(hdcMem);
+	ReleaseDC(hWnd, hdc);
+}
+
